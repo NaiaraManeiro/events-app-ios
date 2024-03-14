1bfd92b691fba8009c64bb2e846aa8498555f087
NTS <ntsdesarrollo@gmail.com>
2024-03-14 16:29:37 +0100

    Primer commit

---
+++ event-app-ios/event_app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  event_app_iosApp.swift
+//  event-app-ios
+//
+//  Created by Naiara Maneiro on 14/3/24.
+//
+
+import SwiftUI
+
+@main
+struct event_app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
